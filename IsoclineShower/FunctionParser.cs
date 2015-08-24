f1bc7f6b4e8fa21e19fb047b6d4e8ffb9a6f3422
Suhoy95 <suhoy95@gmail.com>
2015-08-24 19:57:10 +0400

    Parse Variable and Constant

--- IsoclineShower/FunctionParser.cs
+++ IsoclineShower/FunctionParser.cs
@@ -9,9 +9,53 @@ namespace IsoclineShower
 {
     public class FunctionParser
     {
+        // Принцып перебора символов будет как перевод в польскую запись
+        // http://e-maxx.ru/algo/expressions_parsing
+        //
+
         public static INode Parse(String expression)
         {
-            return new Constant(1.0);
+            var statements = new List<INode>();
+
+            for (var i = 0; i < expression.Length; i++)
+            {
+                if (Char.IsDigit(expression[i]))
+                {
+                    statements.Add(GetNumber(expression, ref i));
+                }
+                else if(Char.IsLetter(expression[i]))
+                {
+                    statements.Add(GetVariable(expression, ref i));
+                }
+
+            }
+
+            return statements.Last();
+        }
+
+        
+
+        private static Constant GetNumber(String expression, ref int i)
+        {
+            int position = i;
+            while (i < expression.Length && 
+                    (Char.IsDigit(expression[i]) || expression[i] == ','))
+                i++;
+
+            var number = expression.Substring(position, i - position);
+
+            return new Constant(double.Parse(number));
+        }
+
+        private static Variable GetVariable(string expression, ref int i)
+        {
+            int position = i;
+            while (i < expression.Length && Char.IsLetter(expression[i]) )
+                i++;
+
+            var variableName = expression.Substring(position, i - position);
+
+            return new Variable(variableName);
         }
     }
 }
--- IsoclineTests/FunctionParser_Test.cs
+++ IsoclineTests/FunctionParser_Test.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using IsoclineShower;
@@ -9,11 +10,21 @@ namespace IsoclineTests
     [TestClass]
     public class FunctionParser_Test
     {
+        private static double delta = Math.Pow(10, -8);
+
         [TestMethod]
-        public void theParseConstant()
+        public void ParseConstant()
         {
-            var node = FunctionParser.Parse("12.5");
-            Assert.AreEqual(12.5, node.Value());
+            var node = FunctionParser.Parse("12,5");
+            Assert.AreEqual(12.5, node.Value(), delta);
+        }
+
+        [TestMethod]
+        public void ParseVariable()
+        {
+            var node = FunctionParser.Parse("x");
+            var variables = new Dictionary<String, double>() {{"x", 12.5}};
+            Assert.AreEqual(12.5, node.Value(variables));
         }
     }
 }
